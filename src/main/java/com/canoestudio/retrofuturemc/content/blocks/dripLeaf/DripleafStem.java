07ac89fc4c02a40e493b1fd2ea1ded4106f0f4a9
Lonelyxiya <3438505487@qq.com>
2025-03-16 07:06:08 +0800

    remove

--- src/main/java/com/canoestudio/retrofuturemc/content/blocks/dripLeaf/DripleafStem.java
+++ src/main/java/com/canoestudio/retrofuturemc/content/blocks/dripLeaf/DripleafStem.java
@@ -43,7 +43,6 @@ public class DripleafStem extends BlockBush implements IHasModel, IGrowable
         setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.SOUTH));
 
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
     @Override
