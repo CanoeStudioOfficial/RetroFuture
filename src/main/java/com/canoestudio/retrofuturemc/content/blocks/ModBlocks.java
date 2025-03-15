097eeacb9ad1ff2db239e14234f506141f02d072
Lonelyxiya <3438505487@qq.com>
2025-03-16 05:31:45 +0800

    cavevine updata

--- src/main/java/com/canoestudio/retrofuturemc/content/blocks/ModBlocks.java
+++ src/main/java/com/canoestudio/retrofuturemc/content/blocks/ModBlocks.java
@@ -1,6 +1,8 @@
 package com.canoestudio.retrofuturemc.content.blocks;
 
 import com.canoestudio.retrofuturemc.content.BlockCreator;
+import com.canoestudio.retrofuturemc.content.blocks.CaveVine.CaveVine;
+import com.canoestudio.retrofuturemc.content.blocks.CaveVine.CaveVinePlant;
 import com.canoestudio.retrofuturemc.content.blocks.dripLeaf.BigDripleaf;
 import com.canoestudio.retrofuturemc.content.blocks.dripLeaf.DripleafStem;
 import com.canoestudio.retrofuturemc.content.blocks.dripLeaf.SmallDripleaf;
@@ -18,5 +20,8 @@ public class ModBlocks {
     public static final Block SMALL_DRIPLEAF = new SmallDripleaf();
     public static final Block BIG_DRIPLEAF = new BigDripleaf();
     public static final Block DRIPLEAF_STEM = new DripleafStem();
+
+    public static final Block CAVE_VINE_PLANT = new CaveVinePlant("cave_vines_plant");
+    public static final Block CAVE_VINE = new CaveVine("cave_vines");
 
 }
