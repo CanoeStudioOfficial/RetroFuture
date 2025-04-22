5c2fa14a16e169e31d0e5215f3f738ace03dd85a
Lonelyxiya <3438505487@qq.com>
2025-04-22 16:34:19 +0800

    Update MossCarpetCreator.java

--- src/main/java/com/canoestudio/retrofuturemc/contents/MossCarpetCreator.java
+++ src/main/java/com/canoestudio/retrofuturemc/contents/MossCarpetCreator.java
@@ -7,6 +7,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -15,10 +16,12 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.block.IGrowable;
+import java.util.Random;
 
 import static com.canoestudio.retrofuturemc.contents.tab.CreativeTab.CREATIVE_TABS;
 
-public class MossCarpetCreator extends Block {
+public class MossCarpetCreator extends Block implements IGrowable {
     protected static final AxisAlignedBB CARPET_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0625D, 1.0D);
 
     public MossCarpetCreator(String name) {
@@ -33,6 +36,52 @@ public class MossCarpetCreator extends Block {
         ModBlocks.BLOCKITEMS.add(new ItemBlock(this).setRegistryName(name.toLowerCase()));
     }
 
+    // 实现IGrowable接口方法
+    @Override
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        // 在周围3x3范围内随机生成苔藓地毯
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(rand.nextFloat() < 0.5F) { // 50%的几率生成
+                    BlockPos targetPos = pos.add(i, 0, j);
+
+                    // 检查目标位置是否适合放置苔藓地毯
+                    if(worldIn.isAirBlock(targetPos) && canPlaceBlockAt(worldIn, targetPos)) {
+                        worldIn.setBlockState(targetPos, this.getDefaultState(), 3);
+                    }
+                }
+            }
+        }
+
+        // 有几率在下方生成苔藓块
+        if(rand.nextFloat() < 0.2F) { // 20%的几率
+            BlockPos belowPos = pos.down();
+            IBlockState belowState = worldIn.getBlockState(belowPos);
+
+            if(isMossable(belowState)) {
+                worldIn.setBlockState(belowPos, ModBlocks.MOSS_BLOCK.getDefaultState(), 3);
+            }
+        }
+    }
+
+    private boolean isMossable(IBlockState state) {
+        Block block = state.getBlock();
+        return block == Blocks.STONE || block == ModBlocks.DeepSlate ||
+                block == Blocks.MYCELIUM || block == Blocks.DIRT ||
+                block == Blocks.GRASS || block == Blocks.COBBLESTONE;
+    }
+
+    // 原有方法保持不变
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return CARPET_AABB;
@@ -78,17 +127,16 @@ public class MossCarpetCreator extends Block {
 
     @Override
     public int getFlammability(IBlockAccess world, BlockPos pos, net.minecraft.util.EnumFacing face) {
-        return 20; // 与原版植物相同的可燃性
+        return 20;
     }
 
     @Override
     public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, net.minecraft.util.EnumFacing face) {
-        return 60; // 与原版植物相同的燃烧速度
+        return 60;
     }
 
     @Override
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
-        // 苔藓地毯有轻微的减速效果
         if (!worldIn.isRemote) {
             entityIn.motionX *= 0.95D;
             entityIn.motionZ *= 0.95D;
