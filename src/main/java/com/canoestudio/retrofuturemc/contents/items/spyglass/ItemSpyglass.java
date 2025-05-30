170711a568b6c6d75b0df567fceebefcada93eff
Lonelyxiya <3438505487@qq.com>
2025-05-30 11:29:21 +0800

    1

--- src/main/java/com/canoestudio/retrofuturemc/contents/items/spyglass/ItemSpyglass.java
+++ src/main/java/com/canoestudio/retrofuturemc/contents/items/spyglass/ItemSpyglass.java
@@ -2,54 +2,52 @@ package com.canoestudio.retrofuturemc.contents.items.spyglass;
 
 
 
-import com.canoestudio.retrofuturemc.contents.items.ModItems;
 import com.canoestudio.retrofuturemc.retrofuturemc.Tags;
-import com.canoestudio.retrofuturemc.sounds.ModSoundEvent;
-import com.canoestudio.retrofuturemc.sounds.RetroFuturemcSoundEvents;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-
-import static com.canoestudio.retrofuturemc.contents.tab.CreativeTab.CREATIVE_TABS;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemSpyglass extends Item {
 
-    public ItemSpyglass(String name)
-    {
+
+    public ItemSpyglass(String name) {
+        setMaxDamage(0);
+        setMaxStackSize(1);
         setTranslationKey(Tags.MOD_ID + "." + name.toLowerCase());
         setRegistryName(name.toLowerCase());
-        setMaxStackSize(1);
+        setCreativeTab(CreativeTabs.TOOLS);
+        setFull3D();
+    }
 
-        setCreativeTab(CREATIVE_TABS);
-
-        ModItems.ITEMS.add(this);
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.BOW;
     }
 
     @Override
     public int getMaxItemUseDuration(ItemStack stack) {
-        return 1200;
+        return 72000;
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-
+        ItemStack itemstack = player.getHeldItem(hand);
         player.setActiveHand(hand);
-        ItemStack stack = player.getHeldItem(hand);
-        player.playSound(RetroFuturemcSoundEvents.SPYGLASS_USE, 1, 1);
-        if (player instanceof EntityPlayerMP) player.addStat(StatList.getObjectUseStats(stack.getItem()));
-        return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
+        return new ActionResult<>(EnumActionResult.SUCCESS, itemstack);
     }
 
-    @Override
-    public void onPlayerStoppedUsing(ItemStack stack, World world, EntityLivingBase entity, int tickCount) {
-        entity.playSound(RetroFuturemcSoundEvents.SPYGLASS_STOP_USING, 1, 1);
-    }
 
 }
