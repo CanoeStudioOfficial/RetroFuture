855477ea700fbe88216f186db484f55eee148d7b
Lonelyxiya <3438505487@qq.com>
2026-03-21 12:17:41 +0800

    Update BigDripleaf.java

--- src/main/java/com/canoestudio/retrofuturemc/contents/blocks/dripLeaf/BigDripleaf.java
+++ src/main/java/com/canoestudio/retrofuturemc/contents/blocks/dripLeaf/BigDripleaf.java
@@ -4,6 +4,7 @@ import com.canoestudio.retrofuturemc.contents.blocks.ModBlocks;
 import com.canoestudio.retrofuturemc.contents.items.ModItems;
 import com.canoestudio.retrofuturemc.retrofuturemc.Tags;
 import com.canoestudio.retrofuturemc.sounds.ModSoundHandler;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
@@ -19,6 +20,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.Random;
@@ -27,7 +29,7 @@ import static com.canoestudio.retrofuturemc.contents.tab.CreativeTab.CREATIVE_TA
 
 public class BigDripleaf extends BlockBush implements IGrowable {
     public static final String name = "Big_Dripleaf";
-public class BigDripleaf extends BlockBush implements IGrowable {
+    public static final SoundType DRIPLEAF = new SoundType(1.0F, 1.0F, ModSoundHandler.BLOCK_BIG_DRIPLEAF_BREAK, ModSoundHandler.BLOCK_BIG_DRIPLEAF_STEP, ModSoundHandler.BLOCK_BIG_DRIPLEAF_PLACE, ModSoundHandler.BLOCK_BIG_DRIPLEAF_HIT, ModSoundHandler.BLOCK_BIG_DRIPLEAF_FALL);
 
     protected static final AxisAlignedBB HALF_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D);
     protected static final AxisAlignedBB DRIP_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.9375D, 1.0D);
@@ -57,6 +59,8 @@ public class BigDripleaf extends BlockBush implements IGrowable {
         this.setTickRandomly(true);
 
         ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+
         setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.SOUTH).withProperty(TILT, EnumTilt.NONE));
     }
 
